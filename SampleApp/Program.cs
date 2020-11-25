e2670e64300646a4fac4c987c9b687d28719ca1a
Vishnu Kumar p s <vishnukumarps2020@outlook.com>
2020-11-25 12:47:23 +0530

    test

---
+++ SampleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            HttpRequestMessage msg = new HttpRequestMessage(HttpMethod.Get,
+                 "https://services.marinetraffic.com/api/exportvesseltrack/bad4c195e465b4c64a3db7287814e2bd513694c8/v:2/period:daily/days:3/mmsi:241486000");
+
+            HttpResponseMessage resp = client.SendAsync(msg).Result;
+            string val = resp.Content.ReadAsStringAsync().Result;
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(val);
+
+            //var opResult = JsonConvert.DeserializeObject<POSITION>(val);
+
+            string json = JsonConvert.SerializeXmlNode(doc);
+             var   allStudents = JsonConvert.DeserializeObject<List<VesselTrack>>(json.ToString());
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SampleApp/VesselTrack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp
+{
+    public class VesselTrack
+    {
+        public string POSITION { get; set; }
+      
+    }
+
+    class POSITION
+    {
+        public string MMSI { get; set; }
+        public string STATUS { get; set; }
+        public string SPEED { get; set; }
+        public string LON { get; set; }
+        public string LAT { get; set; }
+        public string COURCE { get; set; }
+        public string HEADING { get; set; }
+
+        public string SHIP_ID { get; set; }
+
+        public DateTime TIMESTAMP { get; set; }
+
+    }
+}
